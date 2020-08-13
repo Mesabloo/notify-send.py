935360b2f8563d3100f4cd663e7349ebbd81128a
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-08-13 14:31:44 +0200

    create setup.py for use with setuptools

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!python
+
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="notify-send",
+    version="1.2.3",
+    packages=find_namespace_packages(where='notify_send_py'),
+    install_requires=[
+        "dbus-python==1.2.16",
+        "notify2==0.3.1",
+        "pygobject==3.34.0"
+    ],
+    package_dir={'': 'notify_send_py'},
+    entry_points={
+        'console_scripts': [
+            'notify-send = notify-send.main:main'
+        ]
+    },
+    python_requires=">=3.6"
+)
